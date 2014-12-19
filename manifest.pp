421f81fb5a2d83ad0691bfc7740ac32388833c21
Pedro Rosanes <prosanes@gmail.com>
2014-12-19 14:22:37 -0200

    log stash and elasticsearch from a proper yum repo + logstash.conf

--- manifest.pp
+++ manifest.pp
@@ -1,24 +1,59 @@
+yumrepo { 'logstash' :
+	name => 'logstash-repository-for-1.4.x-packages',
+	baseurl => 'http://packages.elasticsearch.org/logstash/1.4/centos',
+	gpgcheck => 1,
+	gpgkey => 'http://packages.elasticsearch.org/GPG-KEY-elasticsearch',
+	enabled => 1,
+}
+
+$logstash_init_hash = {
+	'START' => 'true'
+}
+
 class { 'logstash':
-	package_url => 'http://download.elasticsearch.org/logstash/logstash/packages/centos/logstash-1.3.3-1_centos.noarch.rpm',
 	java_install => true,
+	ensure => 'present',
+	#ensure => 'absent',
+	status => 'running',
+	restart_on_change => true,
+	#configdir => '/etc/logstash/conf.d',
+	init_defaults => $logstash_init_hash,
+}
+
+file { 'logstash.conf':
+	source => '/etc/puppet/modules/logstash/files/logstash_basic.conf',
+	path => '/etc/logstash/conf.d/logstash.conf',
+	ensure => present,
+	#ensure => absent,
+	force => true,
+}
+
+yumrepo { 'elasticsearch' :
+	name => 'elasticsearch-repository-for-1.1.x-packages',
+	baseurl => 'http://packages.elasticsearch.org/elasticsearch/1.1/centos',
+	gpgcheck => 1,
+	gpgkey => 'http://packages.elasticsearch.org/GPG-KEY-elasticsearch',
+	enabled => 1,
 }
 
 class { 'elasticsearch':
-  autoupgrade => true,
-  package_url => 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.1.noarch.rpm',
+  version => '1.1.1-1',
+  ensure => 'present',
+  #ensure => 'absent',
+  java_install => true,
   datadir => '/var/lib/elasticsearch-data',
-  status => 'enabled',
+  status => 'running',
+  restart_on_change => true,
+  config => { 'cluster.name' => 'elasticsearch' }
 }
 
-elasticsearch::instance { 'es-01': }
+elasticsearch::instance { 'es-01':
+	ensure => 'present',
+	status => 'running',
+	config => { 'node.name' => 'es-01' }
+}
 
 elasticsearch::plugin { 'lmenezes/elasticsearch-kopf':
   module_dir => 'kopf',
   instances  => 'es-01'
 }
-
-
-#logstash::configfile { 'filter_apache':
-#	template => 'logstash/filter_apache.file.erb',
-#	order => 30
-#}
